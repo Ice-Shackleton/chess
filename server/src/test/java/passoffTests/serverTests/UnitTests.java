6f099cd0357b165bf7e4143bc4578e3c84a91dcc
Ice-Shackleton <ernestshackleton77@gmail.com>
2024-03-08 22:50:06 -0700

    All tests are passing; last push before autograder.

---
+++ server/src/test/java/passoffTests/serverTests/UnitTests.java
@@ -0,0 +1,25 @@
+package passoffTests.serverTests;
+
+import chess.ChessGame;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class UnitTests {
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void testClear(){
+
+        assertDoesNotThrow(() -> clearService. );
+    }
+}
